04cbf6d0bd8eef85ed6e0b2fbeff1ca0d654957a
eric-boudin <eric.boudin@etu.univ-cotedazur.fr>
2021-02-17 16:05:36 +0100

    Add missing files

---
+++ TD2/Exo5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(mymethod(args));
+        }
+
+        static string mymethod(string[] args)  
+        {   if (args.Length >= 2)
+                return "<HTML><BODY> Hello " + args[0] + " et " + args[1] + ". Generer depuis un programme externe.</BODY></HTML>";
+            else
+                return "Not enough args";
+        }
+    }
+}
